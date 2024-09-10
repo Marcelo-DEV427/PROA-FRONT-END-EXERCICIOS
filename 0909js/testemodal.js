736c5b9ab7635e4e488b4434e96b81c6468ca987
Marcelo-DEV427 <marceloalvescorp@gmail.com>
2024-09-10 16:25:28 -0300

    att

---
+++ 0909js/testemodal.js
@@ -0,0 +1,19 @@
+function abrir_modal(){
+    let modal = document.querySelector(".modal")
+    let janela = document.querySelector(".janela")
+    let imagem = document.querySelector(".modal")
+
+    .modal.style.visibility="visible"
+    .janela.escura.style.visibility="visible"
+    .imagem.style.visibility="visible"
+    
+}
+function fechar_modal(){
+    let modal = document.querySelector(".modal")
+    let janela = document.querySelector(".janela")
+    let imagem = document.querySelector(".modal")
+
+    .modal.style.visibility="hidden"
+    .janela.escura.style.visibility="hidden"
+    .imagem.style.visibility="hidden"
+}
